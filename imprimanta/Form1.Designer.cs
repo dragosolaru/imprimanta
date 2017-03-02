826b516cbac9e1eb79474e09c42084cadd826937
Dragos Andrei Olaru <DragosAndreiOlaru@gmail.com>
2017-03-02 12:09:02 +0200

    ok

--- imprimanta/Form1.Designer.cs
+++ imprimanta/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPrintText = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPrintText
+            // 
+            this.btnPrintText.Location = new System.Drawing.Point(100, 176);
+            this.btnPrintText.Name = "btnPrintText";
+            this.btnPrintText.Size = new System.Drawing.Size(75, 23);
+            this.btnPrintText.TabIndex = 0;
+            this.btnPrintText.Text = "Print Text";
+            this.btnPrintText.UseVisualStyleBackColor = true;
+            this.btnPrintText.Click += new System.EventHandler(this.btnPrintText_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(244, 130);
+            this.textBox.TabIndex = 1;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btnPrintText);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPrintText;
+        private System.Windows.Forms.TextBox textBox;
     }
 }
 
--- imprimanta/Form1.cs
+++ imprimanta/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,35 @@ namespace imprimanta
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnPrintText_Click(object sender, EventArgs e)
+        {
+
+            string sa = "string to print";
+            string s = textBox.Text;
+           
+            PrintDocument p = new PrintDocument();
+            p.PrintPage += delegate (object sender1, PrintPageEventArgs e1)
+            {
+                e1.Graphics.DrawString(s, new Font("Times New Roman", 12), new SolidBrush(Color.Black), new RectangleF(0, 0, p.DefaultPageSettings.PrintableArea.Width, p.DefaultPageSettings.PrintableArea.Height));
+
+            };
+            try
+            {
+                p.Print();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Exception Occured While Printing", ex);
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
