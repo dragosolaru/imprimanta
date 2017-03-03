bc11ad930ef24c90b807786aa63eb77a91541946
Dragos Andrei Olaru <DragosAndreiOlaru@gmail.com>
2017-03-03 16:13:28 +0200

    ok 1

--- imprimanta/Form1.Designer.cs
+++ imprimanta/Form1.Designer.cs
@@ -29,12 +29,26 @@
         private void InitializeComponent()
         {
             this.btnPrintText = new System.Windows.Forms.Button();
-            this.textBox = new System.Windows.Forms.TextBox();
+            this.textBoxNumeCompanie = new System.Windows.Forms.TextBox();
+            this.textBoxSuma = new System.Windows.Forms.TextBox();
+            this.textBoxAdresa = new System.Windows.Forms.TextBox();
+            this.textBoxCUI = new System.Windows.Forms.TextBox();
+            this.textBoxSeria = new System.Windows.Forms.TextBox();
+            this.textBoxNr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxNumeAgent = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnPrintText
             // 
-            this.btnPrintText.Location = new System.Drawing.Point(100, 176);
+            this.btnPrintText.Location = new System.Drawing.Point(13, 227);
             this.btnPrintText.Name = "btnPrintText";
             this.btnPrintText.Size = new System.Drawing.Size(75, 23);
             this.btnPrintText.TabIndex = 0;
@@ -42,21 +56,156 @@
             this.btnPrintText.UseVisualStyleBackColor = true;
             this.btnPrintText.Click += new System.EventHandler(this.btnPrintText_Click);
             // 
-            // textBox
+            // textBoxNumeCompanie
             // 
-            this.textBox.Location = new System.Drawing.Point(12, 12);
-            this.textBox.Multiline = true;
-            this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(244, 130);
-            this.textBox.TabIndex = 1;
-            this.textBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBoxNumeCompanie.Location = new System.Drawing.Point(100, 62);
+            this.textBoxNumeCompanie.Multiline = true;
+            this.textBoxNumeCompanie.Name = "textBoxNumeCompanie";
+            this.textBoxNumeCompanie.Size = new System.Drawing.Size(172, 19);
+            this.textBoxNumeCompanie.TabIndex = 1;
+            this.textBoxNumeCompanie.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxSuma
+            // 
+            this.textBoxSuma.Location = new System.Drawing.Point(100, 137);
+            this.textBoxSuma.Multiline = true;
+            this.textBoxSuma.Name = "textBoxSuma";
+            this.textBoxSuma.Size = new System.Drawing.Size(172, 19);
+            this.textBoxSuma.TabIndex = 2;
+            // 
+            // textBoxAdresa
+            // 
+            this.textBoxAdresa.Location = new System.Drawing.Point(100, 112);
+            this.textBoxAdresa.Multiline = true;
+            this.textBoxAdresa.Name = "textBoxAdresa";
+            this.textBoxAdresa.Size = new System.Drawing.Size(172, 19);
+            this.textBoxAdresa.TabIndex = 3;
+            // 
+            // textBoxCUI
+            // 
+            this.textBoxCUI.Location = new System.Drawing.Point(100, 87);
+            this.textBoxCUI.Multiline = true;
+            this.textBoxCUI.Name = "textBoxCUI";
+            this.textBoxCUI.Size = new System.Drawing.Size(172, 19);
+            this.textBoxCUI.TabIndex = 4;
+            // 
+            // textBoxSeria
+            // 
+            this.textBoxSeria.Location = new System.Drawing.Point(100, 12);
+            this.textBoxSeria.Multiline = true;
+            this.textBoxSeria.Name = "textBoxSeria";
+            this.textBoxSeria.Size = new System.Drawing.Size(172, 19);
+            this.textBoxSeria.TabIndex = 5;
+            // 
+            // textBoxNr
+            // 
+            this.textBoxNr.Location = new System.Drawing.Point(100, 37);
+            this.textBoxNr.Multiline = true;
+            this.textBoxNr.Name = "textBoxNr";
+            this.textBoxNr.Size = new System.Drawing.Size(172, 19);
+            this.textBoxNr.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Seria Doc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nume Comp";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Numar Doc";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Adresa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Suma";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "CUI";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Agent";
+            // 
+            // textBoxNumeAgent
+            // 
+            this.textBoxNumeAgent.Location = new System.Drawing.Point(100, 162);
+            this.textBoxNumeAgent.Multiline = true;
+            this.textBoxNumeAgent.Name = "textBoxNumeAgent";
+            this.textBoxNumeAgent.Size = new System.Drawing.Size(172, 19);
+            this.textBoxNumeAgent.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxNumeAgent);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNr);
+            this.Controls.Add(this.textBoxSeria);
+            this.Controls.Add(this.textBoxCUI);
+            this.Controls.Add(this.textBoxAdresa);
+            this.Controls.Add(this.textBoxSuma);
+            this.Controls.Add(this.textBoxNumeCompanie);
             this.Controls.Add(this.btnPrintText);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -68,7 +217,21 @@
         #endregion
 
         private System.Windows.Forms.Button btnPrintText;
-        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TextBox textBoxNumeCompanie;
+        private System.Windows.Forms.TextBox textBoxSuma;
+        private System.Windows.Forms.TextBox textBoxAdresa;
+        private System.Windows.Forms.TextBox textBoxCUI;
+        private System.Windows.Forms.TextBox textBoxSeria;
+        private System.Windows.Forms.TextBox textBoxNr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxNumeAgent;
     }
 }
 
--- imprimanta/Form1.cs
+++ imprimanta/Form1.cs
@@ -21,30 +21,97 @@ namespace imprimanta
         private void btnPrintText_Click(object sender, EventArgs e)
         {
 
-            string sa = "string to print";
-            string s = textBox.Text;
-           
+            //string sa = "string to print";
+            //string s = textBoxNumeCompanie.Text;
+            int suma = 0;
+            Int32.TryParse(textBoxSuma.Text, out suma);
+
+            StringBuilder stB = new StringBuilder();
+            stB.Append ("<b>+SC BISTRI-VET SRL+</b> \nStr.Libertatii Nr.13, Bistrita - Nasaud\nCIF RO24321865, J06/ 837 / 2008\nCHITANTA\n");
+            stB.AppendLine("Seria: " + textBoxSeria.Text + "\nNumarul: " + textBoxNr.Text+" / "+DateTime.Now.ToString("dd/MM/yyyy") + "\n\nAm primit de la\n" + textBoxNumeCompanie.Text + "\nCUI: " + textBoxCUI.Text + "\nAdresa: " + textBoxAdresa.Text + "\n");
+            stB.AppendLine("Suma de :" + textBoxSuma.Text + "\nAdica: " + NumberToWords(suma) + " RON\nReprezentand cval Fact. Nr.:\n");
+            stB.AppendLine("Emis la data: " + DateTime.Now.ToString() + "\n" + "de catre: " + textBoxNumeAgent.Text + "\n");
+            stB.AppendLine("Tiparit pe hartie termosensibila.\nA se proteja de caldura si de lumina\ndirecta a soarelui.\n\nwww.bistrivet.ro\n\nCasier, ");
+
             PrintDocument p = new PrintDocument();
             p.PrintPage += delegate (object sender1, PrintPageEventArgs e1)
-            {
-                e1.Graphics.DrawString(s, new Font("Times New Roman", 12), new SolidBrush(Color.Black), new RectangleF(0, 0, p.DefaultPageSettings.PrintableArea.Width, p.DefaultPageSettings.PrintableArea.Height));
+            {   
+                Image imgLogo = Image.FromFile("d://Logo bistrivet2.jpg");
+                e1.Graphics.DrawImage(imgLogo, 0, 0);
+                RectangleF rectangle=  new RectangleF(0, imgLogo.Height, p.DefaultPageSettings.PrintableArea.Width, p.DefaultPageSettings.PrintableArea.Height);
 
+                //adaugam textul
+                e1.Graphics.DrawString(stB.ToString(), new Font("Times New Roman", 6), new SolidBrush(Color.Black), rectangle);
+
+                //adugam stampiluta               
+                Image imgStampiluta = Image.FromFile("d://STAMPILUTA.jpg");
+                e1.Graphics.DrawImage(imgStampiluta, 160, 550);
             };
-            try
+
+
+
+            //deschdem fereastra de dialog pentru imprimare
+            PrintDialog pdi = new PrintDialog();
+            pdi.Document = p;
+            if (pdi.ShowDialog() == DialogResult.OK)
             {
                 p.Print();
             }
-            catch (Exception ex)
+            else
             {
-                throw new Exception("Exception Occured While Printing", ex);
+                MessageBox.Show("Print Cancelled");
             }
-
 
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public static string NumberToWords(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            string words = "";       
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " mii ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " suta ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "si ";
+
+                var unitsMap = new[] { "zero", "o", "doua", "trei", "patru", "cinci", "sase", "sapte", "opt", "noua", "10", "unsprezece", "doisprezece", "treisprezece", "paisrezece", "cinsprezece", "saisprezece", "saptesprezece", "optsprezece", "nouasprezece" };
+                var tensMap = new[] { "zero", "zece", "douazeci", "treizeci", "patruzeci", "cinzeci", "saizeci", "saptezeci", "optezci", "nouazeci" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "si" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
         }
     }
 }
